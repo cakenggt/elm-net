504ecff017f3f4f4a6a498e102504c302c054cbe
Alec Lownes <alownes@wescodist.com>
2017-07-17 13:54:38 -0400

    Move weight lines back and change width

--- NetSvg.elm
+++ NetSvg.elm
@@ -77,21 +77,31 @@ generateWeightLines net width height =
         inputs = List.take net.inputs nodePositions
         hiddens = List.take net.hidden (List.drop net.inputs nodePositions)
         outputs = List.drop (net.inputs+net.hidden) nodePositions
+        nodeWidth = getNodeWidth net width
     in
         List.concat
-        [ generateWeightLinesForSection inputWeights inputs hiddens
-        , generateWeightLinesForSection outputWeights hiddens outputs
+        [ generateWeightLinesForSection inputWeights inputs hiddens nodeWidth
+        , generateWeightLinesForSection outputWeights hiddens outputs nodeWidth
         ]
 
-generateWeightLinesForSection : List Float -> List Coord -> List Coord -> List (Svg a)
-generateWeightLinesForSection weights fromCoords toCoords =
-    List.map2 generateWeightLine
+generateWeightLinesForSection : List Float -> List Coord -> List Coord -> Float -> List (Svg a)
+generateWeightLinesForSection weights fromCoords toCoords nodeWidth =
+    List.map2 (generateWeightLine (nodeWidth/2 + 10))
         (List.concat (List.map (\from -> List.map (\to -> (from, to)) toCoords) fromCoords))
         weights
 
-generateWeightLine : (Coord, Coord) -> Float -> Svg a
-generateWeightLine ((fromx, fromy), (tox, toy)) weight =
+generateWeightLine : Float -> (Coord, Coord) -> Float -> Svg a
+generateWeightLine shorten ((fromx, fromy), (tox, toy)) weight =
     let
         color = if weight < 0 then "grey" else "black"
+        c2 = shorten^2
+        slope = (toy-fromy)/(tox-fromx)
+        diffx = sqrt (c2/(1+slope^2))
+        diffy = slope * diffx
+        toy2 = toy - diffy
+        tox2 = tox - diffx
+        fromy2 = fromy + diffy
+        fromx2 = fromx + diffx
+        width = abs (((((Net.logistic weight) * 2) - 1) * 15) + 2)
     in
-        line [ x1 (toString fromx), y1 (toString fromy), x2 (toString tox), y2 (toString toy), strokeWidth (toString (abs (weight*2))), stroke color] []
+        line [ x1 (toString fromx2), y1 (toString fromy2), x2 (toString tox2), y2 (toString toy2), strokeWidth (toString width), stroke color] []
