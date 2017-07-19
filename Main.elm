301bd4eca0d2476ac5ac63059d97a38134aff062
Alec Lownes <alownes@wescodist.com>
2017-07-19 10:33:10 -0400

    Change indentation

--- Main.elm
+++ Main.elm
@@ -120,147 +120,147 @@ update msg model =
       ({model | net = newNet}, Cmd.none)
 
     Backprop ->
-        let
-            tests = testsToFloats model.tests
-        in
-            ( {model | net = Net.backpropagateSet model.net 0.1 tests model.backpropIter}
-            , Cmd.none)
+      let
+        tests = testsToFloats model.tests
+      in
+        ( {model | net = Net.backpropagateSet model.net 0.1 tests model.backpropIter}
+        , Cmd.none)
 
     TimedBackprop time ->
-        let
-            tests = testsToFloats model.tests
-        in
-            if model.running then
-                ( {model | net = Net.backpropagateSet model.net 1 tests 1}
-                , Cmd.none)
-            else
-                (model, Cmd.none)
+      let
+        tests = testsToFloats model.tests
+      in
+        if model.running then
+          ( {model | net = Net.backpropagateSet model.net 1 tests 1}
+          , Cmd.none)
+        else
+          (model, Cmd.none)
 
     NewBackprop str ->
-        let
-            iter = Result.withDefault 0 (String.toInt str)
-        in
-            ({model | backpropIter = iter}, Cmd.none)
+      let
+        iter = Result.withDefault 0 (String.toInt str)
+      in
+        ({model | backpropIter = iter}, Cmd.none)
 
     UrlChange _ ->
         (model, Cmd.none)
 
     IncInput ->
-        ({model
-        | inputs = model.inputs + 1
-        , tests = List.map (\(input, output) -> (List.append input ["0"], output)) model.tests}
-        , Net.createNetRandom (model.inputs + 1) model.hiddens model.outputs NewNet)
+      ({model
+      | inputs = model.inputs + 1
+      , tests = List.map (\(input, output) -> (List.append input ["0"], output)) model.tests}
+      , Net.createNetRandom (model.inputs + 1) model.hiddens model.outputs NewNet)
 
     DecInput ->
-        let
-            newNum = model.inputs - 1
-            finalNum = if newNum < 1 then 1 else newNum
-            tests = if newNum < 1 then model.tests else List.map (\(input, output) -> (pop input, output)) model.tests
-        in
-            ({model
-            | inputs = finalNum
-            , tests = tests}
-            , Net.createNetRandom finalNum model.hiddens model.outputs NewNet)
+      let
+        newNum = model.inputs - 1
+        finalNum = if newNum < 1 then 1 else newNum
+        tests = if newNum < 1 then model.tests else List.map (\(input, output) -> (pop input, output)) model.tests
+      in
+        ({model
+        | inputs = finalNum
+        , tests = tests}
+        , Net.createNetRandom finalNum model.hiddens model.outputs NewNet)
 
     IncHidden ->
-        ({model
-        | hiddens = model.hiddens + 1}
-        , Net.createNetRandom model.inputs (model.hiddens + 1) model.outputs NewNet)
+      ({model
+      | hiddens = model.hiddens + 1}
+      , Net.createNetRandom model.inputs (model.hiddens + 1) model.outputs NewNet)
 
     DecHidden ->
-        let
-            newNum = model.hiddens - 1
-            finalNum = if newNum < 1 then 1 else newNum
-        in
-            ({model
-            | hiddens = finalNum}
-            , Net.createNetRandom model.inputs finalNum model.outputs NewNet)
+      let
+        newNum = model.hiddens - 1
+        finalNum = if newNum < 1 then 1 else newNum
+      in
+        ({model
+        | hiddens = finalNum}
+        , Net.createNetRandom model.inputs finalNum model.outputs NewNet)
 
     IncOutput ->
-        ({model
-        | outputs = model.outputs + 1
-        , tests = List.map (\(input, output) -> (input, List.append output ["0"])) model.tests}, Net.createNetRandom model.inputs model.hiddens (model.outputs + 1) NewNet)
+      ({model
+      | outputs = model.outputs + 1
+      , tests = List.map (\(input, output) -> (input, List.append output ["0"])) model.tests}, Net.createNetRandom model.inputs model.hiddens (model.outputs + 1) NewNet)
 
     DecOutput ->
-        let
-            newNum = model.outputs - 1
-            finalNum = if newNum < 1 then 1 else newNum
-            tests = if newNum < 1 then model.tests else List.map (\(input, output) -> (input, pop output)) model.tests
-        in
-            ({model
-            | outputs = finalNum
-            , tests = tests}, Net.createNetRandom model.inputs model.hiddens finalNum NewNet)
+      let
+        newNum = model.outputs - 1
+        finalNum = if newNum < 1 then 1 else newNum
+        tests = if newNum < 1 then model.tests else List.map (\(input, output) -> (input, pop output)) model.tests
+      in
+        ({model
+        | outputs = finalNum
+        , tests = tests}, Net.createNetRandom model.inputs model.hiddens finalNum NewNet)
 
     ChangeTest testIndex nodeType nodeIndex str ->
-        let
-          tests = List.indexedMap (\index test ->
-            if index == testIndex then
-                case nodeType of
-                    Input ->
-                        (List.indexedMap (\index node ->
-                            if index == nodeIndex then
-                                str
-                            else
-                                node) (Tuple.first test), Tuple.second test)
-                    Hidden ->
-                        test
-                    Output ->
-                        (Tuple.first test, List.indexedMap (\index node ->
-                            if index == nodeIndex then
-                                str
-                            else
-                                node) (Tuple.second test))
-            else
-                test) model.tests
-        in
-          ({model | tests = tests}, Cmd.none)
+      let
+        tests = List.indexedMap (\index test ->
+          if index == testIndex then
+            case nodeType of
+              Input ->
+                (List.indexedMap (\index node ->
+                  if index == nodeIndex then
+                    str
+                  else
+                    node) (Tuple.first test), Tuple.second test)
+              Hidden ->
+                test
+              Output ->
+                (Tuple.first test, List.indexedMap (\index node ->
+                  if index == nodeIndex then
+                    str
+                  else
+                    node) (Tuple.second test))
+          else
+            test) model.tests
+      in
+        ({model | tests = tests}, Cmd.none)
 
     ToggleRunning ->
-        ({model | running = not model.running}, Cmd.none)
+      ({model | running = not model.running}, Cmd.none)
 
     AddTest ->
-        ({model
-        | tests = List.append model.tests [(List.repeat model.inputs "0", List.repeat model.outputs "0")]}
-        , Cmd.none)
+      ({model
+      | tests = List.append model.tests [(List.repeat model.inputs "0", List.repeat model.outputs "0")]}
+      , Cmd.none)
 
     RemoveTest index ->
-        ({model
-        | tests = List.filterMap (\(i, test) -> if i /= index then Just test else Nothing) (List.indexedMap (,) model.tests)}
-        , Cmd.none)
+      ({model
+      | tests = List.filterMap (\(i, test) -> if i /= index then Just test else Nothing) (List.indexedMap (,) model.tests)}
+      , Cmd.none)
 
 pop : List a -> List a
 pop ls =
-    List.take ((List.length ls)-1) ls
+  List.take ((List.length ls)-1) ls
 
 
 stringToList : String -> List (List String)
 stringToList str =
-    case decodeString (Json.Decode.list (Json.Decode.list Json.Decode.string)) str of
-        Ok ls ->
-            ls
-        Err _ ->
-            [[]]
+  case decodeString (Json.Decode.list (Json.Decode.list Json.Decode.string)) str of
+    Ok ls ->
+      ls
+    Err _ ->
+      [[]]
 
 testsToFloats : List (List String, List String) -> List (List Float, List Float)
 testsToFloats test =
-    List.map (\(input, output) ->
-        (testToFloat input
-        ,testToFloat output)) test
+  List.map (\(input, output) ->
+    (testToFloat input
+    ,testToFloat output)) test
 
 testToFloat : List String -> List Float
 testToFloat test =
-    List.map (\s -> Result.withDefault 0 (String.toFloat s)) test
+  List.map (\s -> Result.withDefault 0 (String.toFloat s)) test
 
 getSizeOfNestedList : String -> Int
 getSizeOfNestedList str =
-    let
-      total = stringToList str
-    in
-      case List.head total of
-        Just ls ->
-          List.length ls
-        Nothing ->
-          0
+  let
+    total = stringToList str
+  in
+    case List.head total of
+      Just ls ->
+        List.length ls
+      Nothing ->
+        0
 
 -- SUBSCRIPTIONS
 
@@ -317,4 +317,4 @@ createTest model testIndex test =
 
 createTestInput : Model -> Int -> NodeType -> Int -> String -> Html Msg
 createTestInput model testIndex nodeType nodeIndex val =
-    input [ onInput (ChangeTest testIndex nodeType nodeIndex), value val] []
+  input [ onInput (ChangeTest testIndex nodeType nodeIndex), value val] []
